e9b56432e49b2b5960a56e43def5c082a620f092
saida brito <saidabrito57@gmail.com>
2020-05-19 21:23:33 -0500

    Updated the type of a column on the Critic table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_005711) do
+ActiveRecord::Schema.define(version: 2020_05_20_021958) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_005711) do
 
   create_table "critics", force: :cascade do |t|
     t.string "username"
-    t.string "body"
+    t.text "body"
     t.integer "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
