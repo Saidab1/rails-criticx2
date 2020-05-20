2927a63d98bff7ca761662fe0b4581cebb7c7470
saida brito <saidabrito57@gmail.com>
2020-05-20 16:32:11 -0500

    Added Task 3: Create a new Company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,8 +4,23 @@ class CompaniesController < ApplicationController
     render json: @companies
   end
 
-  def show 
+  def show
     @companies = Company.find(params[:id])
     render json: @companies
-  end 
+  end
+
+  def create
+    @companies = Company.new(company_params)
+    if @companies.save
+      render json: @companies
+    else
+      render json: @companies.errors
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
 end
