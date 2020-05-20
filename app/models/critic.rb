d549b6226b7a2be00f7df536a3ea48d05b0699b3
saida brito <saidabrito57@gmail.com>
2020-05-19 22:57:30 -0500

    Added validations to the critic table

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,6 @@
 class Critic < ApplicationRecord
-  belongs_to :game 
+  belongs_to :game
+  validates :username, :game, presence: true
+  validates :username, length: {in: 2..12}, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :title, :body, presence: {message: "Please write something!"}
 end
